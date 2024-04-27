7b4f6723e7d56ad33a2b5654da3c1deb3c227036
Volokhova Svetlana <volokhovasv@nspk.ru>
2024-04-28 00:20:55 +0300

    feat: add js logic

--- js/edit-form.js
+++ js/edit-form.js
@@ -7,4 +7,44 @@ const contact = {
   job_title: "Manager",
 };
 
-console.log(`${contact.name} ${contact.lastname}, ${contact.job_title}`);
+window.addEventListener("DOMContentLoaded", () => {
+  function setFormValue(id, attrValue) {
+    document.getElementById(id).setAttribute("value", attrValue);
+  }
+
+  function createAlert(text) {
+    const div = document.createElement("div");
+    div.className = "alert";
+
+    const alertClose = document.createElement("span");
+    alertClose.className = "alert_close";
+    alertClose.innerText = "X";
+    
+    const url = document.createElement("a");
+    url.setAttribute("href", "table.html");
+  
+    const alertText = document.createElement("span");
+    alertText.className = "alert_text";
+    alertText.innerText = text;
+
+    url.append(alertClose);
+    div.append(url, );
+    
+    return div;
+  }
+
+  document.querySelector(
+    ".profile_name"
+  ).innerText = `${contact.name} ${contact.lastname}`;
+
+  for (var key of Object.keys(contact)) {
+    setFormValue(key, contact[key]);
+  }
+
+  document.querySelector(".button_edit").onclick = function () {
+    const body = document.querySelector("body");
+    // body.style = 'background-color: #000000B2;';
+    
+    body.append(createAlert("Contact was changed"));
+  };
+});
