413479a25fb592caca8ef729c68ba2c2a44027e1
Volokhova Svetlana <volokhovasv@nspk.ru>
2024-04-27 20:39:07 +0300

    feat: create html pages

---
+++ js/edit-form.js
@@ -0,0 +1,10 @@
+const contact = {
+  name: "Test",
+  lastname: "Testov",
+  phone: "+79000000111",
+  email: "test@gmail.com",
+  company_name: "NSPK",
+  job_title: "Manager",
+};
+
+console.log(`${contact.name} ${contact.lastname}, ${contact.job_title}`);
---
+++ js/table.js
@@ -0,0 +1,27 @@
+let contact_one = {
+  name: "Test",
+  lastname: "Testov",
+  phone: "+79000000111",
+  email: "test@gmail.com",
+  company_name: "NSPK",
+  job_title: "Manager",
+};
+
+let contact_two = {
+  name: "Svetlana",
+  lastname: "Volokhova",
+  phone: "+79000000000",
+  email: "sv@gmail.com",
+  company_name: "NSPK",
+  job_title: "Developer",
+};
+
+let contact_three = {
+  name: "Petr",
+  lastname: "Petrov",
+  phone: "+79000000123",
+  email: "petrov@ya.ru",
+  company_name: "Sber",
+  job_title: "HR",
+};
+
