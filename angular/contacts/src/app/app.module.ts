33f9f5a67cec9e35c5b532627c4700725bfe4650
AdalizaEntropy <adalizaentropy@yandex.ru>
2024-05-17 14:43:46 +0300

    feat: added all components to project

--- angular/contacts/src/app/app.module.ts
+++ angular/contacts/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeadComponent } from './core/head/head.component';
 import { MenuComponent } from './core/menu/menu.component';
+import { AddContactComponent } from './components/add-contact/add-contact.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { AddFormContactComponent } from './components/add-form-contact/add-form-contact.component';
+import { ListContactsComponent } from './components/list-contacts/list-contacts.component';
+import { EditContactComponent } from './pages/contacts/edit-contact/edit-contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadComponent,
-    MenuComponent
+    MenuComponent,
+    AddContactComponent,
+    ContactsComponent,
+    AddFormContactComponent,
+    ListContactsComponent,
+    EditContactComponent
   ],
   imports: [
     BrowserModule,
